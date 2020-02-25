d4e2cdb9dc08765a7978cd7590b25b3405991175
Guilherme <guilhermebromonschenkel@gmail.com>
2020-02-24 23:11:36 -0300

    chore(*): finish user crud methods

--- full-stack-project/back-end/src/resolvers/Mutation/User.js
+++ full-stack-project/back-end/src/resolvers/Mutation/User.js
@@ -1,13 +1,72 @@
-// let { users, nextId } = require("../../database/mock")
+const database = require("../../lib/database")
 
 module.exports = {
-	createUser(_, { data: { name, email, age } }) {
-		// TODO
+	async createUser(_, { data }) {
+		const user = database("users").where({ email: data.email }).first()
+
+		if (user) {
+			throw new Error("User already exists!")
+		}
+
+		const profileIds = []
+
+		if (data.profiles && data.profiles.length) {
+			for (let key of Object.keys(data.profiles)) {
+				if (key === "id") {
+					profileIds.push(data.profiles[key])
+				} else if (key === "name") {
+					const { id } = await database("profiles").where({ name: data.profiles[key] }).first()
+					profileIds.push(id)
+				}
+			}
+
+			delete data.profiles
+		}
+
+		const [id] = await database("users").insert({ email: data.email, password: data.password, name: data.name}).returning("id")
+
+		await Promise.all(profileIds.map(profile_id => database("users_profiles").insert({ profile_id, user_id: id })))
+
+		const newUser = await database("users").where({ id }).first()
+
+		return newUser
 	},
-	deleteUser(_, { id }) {
-		// TODO
+	async deleteUser(_, { id }) {
+		const user = await database("users").where({ id }).first()
+
+		if (user) {
+			await database("users_profiles").where({ user_id: id }).delete()
+			await database("users").where({ id }).delete()
+
+			return user
+		} else {
+			return null
+		}
 	},
-	changeUser(_, { data: { name, email, age } }) {
-		// TODO
+	async changeUser(_, { id, data }) {
+		const user = await database("users").where({ id }).first()
+
+		if (user) {
+			if (data.profiles && data.profiles.length) {
+				await database("users_profiles").where({ user_id: id }).delete()
+
+				for (let key of Object.keys(data.profiles)) {
+					if (key === "id") {
+						profileIds.push(data.profiles[key])
+					} else if (key === "name") {
+						const { id } = await database("profiles").where({ name: data.profiles[key] }).first()
+						profileIds.push(id)
+					}
+				}
+	
+				delete data.profiles
+			}
+
+			await database("users").where({ id }).update(data)
+
+			return { ...user, ...data }
+		} else {
+			return null
+		}
 	}
 }
